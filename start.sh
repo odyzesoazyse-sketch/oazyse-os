4bedbc7689cf62b5e1bbd8cf30e20572584f36e0
oazyse <dev@oazyse.os>
2026-04-07 23:25:05 +0500

    fix: make start.sh generic and cross-platform

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-export PATH="/usr/local/bin:/usr/local/Cellar/node/25.5.0/bin:$PATH"
 cd "$(dirname "$0")"
-exec /usr/local/Cellar/node/25.5.0/bin/node /usr/local/bin/npx ts-node src/server/Server.ts
+exec npx ts-node src/server/Server.ts
